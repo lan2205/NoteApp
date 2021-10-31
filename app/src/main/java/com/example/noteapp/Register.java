e8c27713619f3369c8620e38df2c0b5bea978b88
lan2205 <19521745@gm.uit.edu.vn>
2021-10-31 20:28:23 +0700

    NoteApp

--- app/src/main/java/com/example/noteapp/Register.java
+++ app/src/main/java/com/example/noteapp/Register.java
@@ -3,15 +3,12 @@ package com.example.noteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-
-
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
