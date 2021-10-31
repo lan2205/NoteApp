7f6192cf618cf14a2fcbec6f77b09b51030e1fee
lan2205 <19521745@gm.uit.edu.vn>
2021-10-31 22:15:36 +0700

    NoteApp

--- app/src/main/java/com/example/noteapp/Register.java
+++ app/src/main/java/com/example/noteapp/Register.java
@@ -2,13 +2,42 @@ package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+
+    private TextView tv_signin;
+    private Button btn_register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tv_signin = (TextView) findViewById(R.id.tv_signin);
+        btn_register = (Button) findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this,Register1.class);
+                startActivity(intent);
+            }
+        });
+
+
+        tv_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this,Login.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/noteapp/Register1.java
+++ app/src/main/java/com/example/noteapp/Register1.java
@@ -6,25 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+public class Register1 extends AppCompatActivity {
 
-    private TextView tv_signup;
+    private Button btn_continue_reg1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register1);
 
-        tv_signup = (TextView) findViewById(R.id.tv_signup);
-        tv_signup.setOnClickListener(new View.OnClickListener() {
+        btn_continue_reg1 = (Button) findViewById(R.id.btn_continue_reg1);
+        btn_continue_reg1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this,Register.class);
+                Intent intent = new Intent(Register1.this, Register2.class);
                 startActivity(intent);
             }
         });
-
     }
 }
