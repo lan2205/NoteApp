86dbae296aee3eb60bf5aa10652dbbe6f1210ab7
NTQuii <19522105@gm.uit.edu.vn>
2021-11-01 09:35:22 +0700

    Qui

--- app/src/main/java/com/example/noteapp/ForgotPassword.java
+++ app/src/main/java/com/example/noteapp/ForgotPassword.java
@@ -2,13 +2,39 @@ package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ForgotPassword extends AppCompatActivity {
+    private Button btn_continue_forgot;
+    private TextView tv_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        tv_signup = (TextView) findViewById(R.id.tv_signup);
+        btn_continue_forgot = (Button) findViewById(R.id.btn_continue_forgot);
+
+        btn_continue_forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPassword.this,ForgotPassword1.class);
+                startActivity(intent);
+            }
+        });
+
+
+        tv_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPassword.this,Login.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/noteapp/ForgotPassword1.java
+++ app/src/main/java/com/example/noteapp/ForgotPassword1.java
@@ -2,13 +2,27 @@ package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ForgotPassword1 extends AppCompatActivity {
+    private Button btn_continue_forgot1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password1);
+        btn_continue_forgot1 = (Button) findViewById(R.id.btn_continue_forgot1);
+
+        btn_continue_forgot1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPassword1.this,ForgotPassword2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
