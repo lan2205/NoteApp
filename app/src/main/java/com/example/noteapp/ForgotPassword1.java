8a7f43fa1297166d587f778d8162a2aeff0da85d
TuyenDoan <19522501@gm.uit.edu.vn>
2021-11-01 09:09:15 +0700

    TuyenDoan

---
+++ app/src/main/java/com/example/noteapp/ForgotPassword1.java
@@ -0,0 +1,14 @@
+package com.example.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPassword1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password1);
+    }
+}
