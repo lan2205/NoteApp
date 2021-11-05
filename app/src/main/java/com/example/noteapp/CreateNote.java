5fcfb9ecec4c690926a1083f8c5ce619bb9e76a0
TuyenDoan <19522501@gm.uit.edu.vn>
2021-11-05 17:08:16 +0700

    tuyendoan

--- app/src/main/java/com/example/noteapp/CreateNote.java
+++ app/src/main/java/com/example/noteapp/CreateNote.java
@@ -2,13 +2,38 @@ package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.noteapp.Home.Home;
 
 public class CreateNote extends AppCompatActivity {
 
+    private ImageView img_backHome;
+    private ImageView img_More;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
+        img_backHome = (ImageView) findViewById(R.id.imageBack);
+        img_More = (ImageView) findViewById(R.id.more_actions);
+
+        img_backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateNote.this, Home.class);
+                startActivity(intent);
+            }
+        });
+        img_More.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateNote.this, CreateNote.class);
+                startActivity(intent);
+            }
+        });
     }
 }
