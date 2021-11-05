13502490d749182b349933ebe1b177b7660c2eb9
TuyenDoan <19522501@gm.uit.edu.vn>
2021-11-05 15:47:28 +0700

    tuyendoan

--- app/src/main/java/com/example/noteapp/CreateNote.java
+++ app/src/main/java/com/example/noteapp/CreateNote.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Note extends AppCompatActivity {
+public class CreateNote extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note);
+        setContentView(R.layout.activity_create_note);
     }
 }
