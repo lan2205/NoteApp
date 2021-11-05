08e94fb9e549fe5fe972f814acb1fc8f54cd5809
lan2205 <19521745@gm.uit.edu.vn>
2021-11-05 17:11:28 +0700

    NoteApp

--- app/src/main/java/com/example/noteapp/Home/Home.java
+++ app/src/main/java/com/example/noteapp/Home/Home.java
@@ -16,11 +16,23 @@ import com.example.noteapp.R;
 public class Home extends AppCompatActivity {
 
     private ImageView img_NewNote;
+    private ImageView ic_menu;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         img_NewNote = (ImageView) findViewById(R.id.newNote);
+        ic_menu = (ImageView) findViewById(R.id.ic_menu);
+
+        ic_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Settings.class);
+                startActivity(intent);
+            }
+        });
 
         img_NewNote.setOnClickListener(new View.OnClickListener() {
             @Override
