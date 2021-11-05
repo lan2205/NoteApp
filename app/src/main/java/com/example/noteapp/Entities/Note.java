fe3a2cd649843111963cc9dfedfe9c4d20789f1d
TuyenDoan <19522501@gm.uit.edu.vn>
2021-11-06 00:25:03 +0700

    tuyendoan

---
+++ app/src/main/java/com/example/noteapp/Entities/Note.java
@@ -0,0 +1,6 @@
+package com.example.noteapp.Entities;
+
+
+
+public class Note {
+}
--- app/src/main/java/com/example/noteapp/Home/CreateNote.java
+++ app/src/main/java/com/example/noteapp/Home/CreateNote.java
@@ -1,18 +1,21 @@
 package com.example.noteapp.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 import com.example.noteapp.R;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNote extends AppCompatActivity {
 
     private ImageView img_backHome;
     private ImageView img_More;
+    private TextView tv_dateTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,7 @@ public class CreateNote extends AppCompatActivity {
 
         img_backHome = (ImageView) findViewById(R.id.imageBack);
         img_More = (ImageView) findViewById(R.id.more_actions);
+        tv_dateTime = (TextView)findViewById(R.id.textDateTime);
 
         img_backHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +39,10 @@ public class CreateNote extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        tv_dateTime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a",Locale.getDefault())
+                .format(new Date())
+        );
+
     }
 }
