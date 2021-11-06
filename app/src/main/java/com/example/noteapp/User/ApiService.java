720123d8956c2cccee23c602d51728ad1f31631b
TuyenDoan <19522501@gm.uit.edu.vn>
2021-11-06 15:04:58 +0700

    tuyendoan

---
+++ app/src/main/java/com/example/noteapp/User/ApiService.java
@@ -0,0 +1,31 @@
+package com.example.noteapp.User;
+
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.ArrayList;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+public interface ApiService {
+
+    Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-mm-dd hh:mm:ss")
+            .create();
+
+    HttpLoggingInterceptor logging = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient().newBuilder().addInterceptor(logging);
+
+    ApiService apiService = new Retrofit.Builder()
+            .baseUrl("https://noteappteam20.azurewebsites.net")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClientBuilder.build())
+            .build()
+            .create(ApiService.class);
+
+    @GET("api/user")
+    Call<ArrayList<User>> getUser();
+}
