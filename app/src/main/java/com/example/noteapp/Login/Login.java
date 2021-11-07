0f426732c1f8da42be7f0746299258bbafb10761
TuyenDoan <19522501@gm.uit.edu.vn>
2021-11-07 10:15:24 +0700

    tuyendoan

--- app/src/main/java/com/example/noteapp/Login/Login.java
+++ app/src/main/java/com/example/noteapp/Login/Login.java
@@ -7,8 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.noteapp.ForgotPassword.ForgotPassword;
 import com.example.noteapp.Home.Home;
 import com.example.noteapp.R;
@@ -20,12 +23,15 @@ public class Login extends AppCompatActivity {
     private TextView tv_forgot;
     private ImageView iv_show_hide_password;
     private Button btN_Login;
+    private EditText Username,Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Username = (EditText) findViewById(R.id.Username);
+        Password = (EditText) findViewById(R.id.Password);
         tv_forgot = (TextView) findViewById(R.id.tv_forgot);
         tv_signup = (TextView) findViewById(R.id.tv_signup);
         iv_show_hide_password = (ImageView) findViewById(R.id.ShowHide_PW);
@@ -50,6 +56,22 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Login.this, Home.class);
                 startActivity(intent);
+            }
+        });
+
+        btN_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String User = "tienxinxan";
+                String pass = "123";
+                if (Username.getText().toString().equals(User) && Password.getText().toString().equals(pass)) {
+                    Toast.makeText(getApplicationContext(), "Successfull! Welcome " +User, Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Login.this, Home.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong account or password", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
